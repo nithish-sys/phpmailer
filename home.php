ab1ed84e79e89aa83743014b6ecfda792f2e1189
nithish-sys <nithishnithin999@gmail.com>
2022-08-07 18:47:38 +0530

    index file change

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>random XKCD</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <h1 class="welcome" style="background-color:black; color:white;">Welcome to XKCD random comics section</h1>
+
+</body>
+
+</html>
